5c6bf23df008378b9b2adffee7945f24520f30bf
Gonzalo Sola <gonzalo.fermin@tata.com.uy>
2022-12-08 20:03:31 -0300

    Agregar expo y algunas dependencias

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const onPressTitle = () => {
+  alert("El dedo Nene");
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyCcontent: "center",
+      }}
+    >
+      <Text
+        style={{
+          alignContent: "flex-start",
+          flex: 2,
+          marginTop: 5,
+          paddingTop: 100,
+          fontSize: 48,
+        }}
+        onPress={onPressTitle}
+      >
+        Fede Te amo
+      </Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

1c07abaf7439efc3dc071b8cf6ca2ce7f9bbc105
Gonzalo Sola <gonzalo.fermin@tata.com.uy>
2022-12-19 17:05:10 -0300

    Agrego comillas que faltaron

--- App.js
+++ App.js
@@ -15,8 +15,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: center,
-    justifyContent: center,
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     alignContent: "flex-start",
